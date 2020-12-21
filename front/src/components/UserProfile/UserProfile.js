fcf5c4c673595189074bd30a7b931d8e2716ebeb
sultanius <timusultano@yandex.ru>
2020-12-21 20:30:37 +0300

    image

--- front/src/components/UserProfile/UserProfile.js
+++ front/src/components/UserProfile/UserProfile.js
@@ -1,44 +1,48 @@
 import React from 'react';
-import {useSelector} from "react-redux";
-import ImageUploader from "react-image-uploader"
+import {useDispatch, useSelector} from "react-redux";
 import {upload} from "./api";
+
 
 
 const UserProfile = () => {
     const user = useSelector(state => state.auth.user)
+    const photo = useSelector(state => state)
+
 
     const [imgPreview, setImgPreview] = React.useState(null)
     const [error, setError] = React.useState(false)
 
+    const dispatch = useDispatch()
     const handleImageChange = (e) => {
         const selected = e.target.files[0]
-        console.log(selected)
         const ALLOWED_TYPES = ["image/png", "image/jpeg", "image/jpg"]
         if(selected && ALLOWED_TYPES.includes(selected.type)){
             console.log('selected')
             let reader = new FileReader()
             reader.onloadend = () => {
                 setImgPreview(reader.result)
+                dispatch({type: 'ADD_PHOTO', payload:reader.result})
+                localStorage.setItem('photo', JSON.stringify(reader.result) )   //чтобы записать данные в локалстор
             }
             reader.readAsDataURL(selected)
         } else {
             setError(true)
             console.log('files not supported')
         }
+
     }
+
     return (
         <div>
            Name: {user && user.name}<br/>
            Mail: {user && user.email}<br/>
+
+           {localStorage.getItem('photo') ? <img className="imgPreview" src={ JSON.parse( localStorage.getItem('photo') )} /> : null}
             <div className="container">
                 {error && <p className="errorMsg">File not supported</p>}
                 <div
-                className="imgPreview"
-                style={{background: imgPreview
-                        ? `url("${imgPreview}") no-repeat center/cover`
-                        : "#131313"
-                }}>
-                    {!imgPreview && (
+               >
+                    {!localStorage.getItem('photo') && (
                         <>
                         <p>Add an image</p>
                         <label htmlFor="fileUpload" className="customFileUpload">
@@ -49,12 +53,8 @@ const UserProfile = () => {
                         </>
                         )}
                 </div>
-                {imgPreview && (
-                    <button onClick={() => setImgPreview(null)}>Remove image</button>
-                )}
+                    <button onClick={() => localStorage.removeItem('photo')}>Remove image</button>
             </div>
-            {/*<input type={"file"}  onChange={handleImageChange}/>*/}
-
         </div>
     );
 };
